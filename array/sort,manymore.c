cd92c229a6833ec5208015c0cbdcd7e28e73dc80
tejppatil <tppatil397@gmail.com>
2023-12-16 00:02:04 +0530

    1512

--- array/sort,manymore.c
+++ array/sort,manymore.c
@@ -1,3 +1,14 @@
 //Write programs to manipulate arrays (sorting, searching, insertion, deletion).
 #include<stdio.h>
 int main()
+{
+    int n,a[n];
+    printf("Enter the size of array: ");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        printf("Enter the element %d in array",i);
+        scanf("%d",&a[i]);
+    }
+    return 0;
+}
