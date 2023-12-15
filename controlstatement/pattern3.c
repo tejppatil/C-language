002f164e5a86c32121a3c3c7e5508ae62e669b6d
tejppatil <tppatil397@gmail.com>
2023-12-15 14:14:33 +0530

    pattern

--- controlstatement/pattern3.c
+++ controlstatement/pattern3.c
@@ -3,15 +3,15 @@
 int main()
 {
     int n;
-    printf("Enter the number(in multiples of 2) of line: ");
+    printf("Enter the number of line: ");
     scanf("%d",&n);
-    for(int i=1;i<=n/2;i++)
+    for(int i=1;i<=(n/2)+1;i++)
     {
         for(int j=1;j<=i;j++)
             printf("*");
             printf("\n");
     }
-    for(int i=n/2;i>0;i--)
+    for(int i=(n/2);i>0;i--)
     {
         for(int j=i;j>0;j--)
             printf("*");
---
+++ functions/powerofnum.c
@@ -0,0 +1,16 @@
+//Write a program that calculates the power of a number using a user-defined function
+#include<stdio.h>
+void power(int m,int n)
+{
+
+}
+int main()
+{
+   int a,b;
+   printf("Enter the Number: ");
+   scanf("%d",&a);
+   printf("Enter the power of number: ");
+   scanf("%d",&b);
+   power(m,n);
+   printf("The value of power is %d",power(m,n))
+}
