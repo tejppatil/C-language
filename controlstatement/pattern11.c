f710a919e0ceffb80b2df21cde93fb9fbe644831
tejppatil <tppatil397@gmail.com>
2023-12-15 13:43:03 +0530

    pattern

--- controlstatement/pattern11.c
+++ controlstatement/pattern11.c
@@ -5,19 +5,19 @@ int main()
     int n;
     printf("Enter the number(in multiples of 2) of line: ");
     scanf("%d",&n);
-    for(int i=n/2;i>=1;i--)
+    for(int i=1;i<=n/2;i++)
     {
         for(int j=n-i;j>=1;j--)
             printf(" ");
-        for(int j=i;j>=1;j--)
+        for(int j=1;j<=i;j++)
             printf("* ");
             printf("\n");
     }
-    for(int i=1;i<=n/2;i++)
+    for(int i=n/2;i>0;i--)
     {
         for(int j=1;j<=n-i;j++)
             printf(" ");
-        for(int j=1;j<=i;j++)
+        for(int j=i;j>0;j--)
             printf("* ");
             printf("\n");
     }
