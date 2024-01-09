f83b372c47e25f86bf17d44cc918e1a3c9a4e76e
tejppatil <tppatil397@gmail.com>
2024-01-10 00:32:36 +0530

    change

--- Practical/2nd.c
+++ Practical/2nd.c
@@ -1,33 +1,33 @@
 //Write a c program where a positive integer is entered through the keyboard and print the binary equivalent of this number using recursion.
 #include <stdio.h>
-void decimalToBinary(int n) 
+void d(int n) 
 {
     if (n > 0) 
     {
-        decimalToBinary(n/2);
+        d(n/2);
         printf("%d", n % 2);
     }
 }
 
 int main() 
 {
-    int decimalNumber;
+    int num;
     printf("Enter a positive integer: ");
-    scanf("%d", &decimalNumber);
+    scanf("%d", &num);
 
-    if (decimalNumber < 0) 
+    if (num < 0) 
     {
         printf("Please enter a positive integer.\n");
         return 1;
     }
     printf("Binary equivalent: ");
-    if (decimalNumber == 0) 
+    if (num == 0) 
     {
         printf("0");
     } 
     else 
     {
-        decimalToBinary(decimalNumber);
+        d(num);
     }
     return 0;
 }
